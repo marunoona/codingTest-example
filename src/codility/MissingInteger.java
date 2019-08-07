691061dad96efa2423177c7fd701c5e9bf1b931a
marunoona <ye_jin92@naver.com>
2019-08-07 19:50:25 +0900

    문제추가

---
+++ src/codility/MissingInteger.java
@@ -0,0 +1,27 @@
+package codility;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MissingInteger {
+    public int solution(int[] A) {
+        //Arrays.sort(A);
+
+        Set<Integer> set = new HashSet<>();
+        for (int a : A) {
+            set.add(a);
+        }
+
+        for (int i = 1; i <= Integer.MAX_VALUE; i++) {
+            if (!set.contains(i))
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        MissingInteger missingInteger = new MissingInteger();
+        int[] A = {1,2,3,6,1,2};
+        System.out.println(missingInteger.solution(A));
+    }
+}
---
+++ src/codility/PermCheck.java
@@ -0,0 +1,21 @@
+package codility;
+
+import java.util.Arrays;
+
+public class PermCheck {
+    public int solution(int[] A) {
+
+        Arrays.sort(A);
+        for (int i = 0; i < A.length; i++) {
+            if (i + 1 != A[i])
+                return 0;
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        PermCheck permCheck = new PermCheck();
+        int[] A = {4,3,2,1};
+        System.out.println(permCheck.solution(A));
+    }
+}
