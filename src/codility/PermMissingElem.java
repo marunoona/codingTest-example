2543272606c8255c7f6936ecaf9b2faa9f9622fb
marunoona <ye_jin92@naver.com>
2019-08-07 19:50:35 +0900

    코드수정

--- src/codility/PermMissingElem.java
+++ src/codility/PermMissingElem.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class PermMissingElem {
     public int solution(int[] A) {
+
         Arrays.sort(A);
         for (int i = 0; i < A.length; i++) {
             if (i + 1 != A[i])
@@ -14,7 +15,7 @@ public class PermMissingElem {
 
     public static void main(String[] args) {
         PermMissingElem permMissingElem = new PermMissingElem();
-        int[] A = {1, 4, 3, 6, 2};
+        int[] A = {1, 2, 3, 5};
         System.out.println(permMissingElem.solution(A));
     }
 }
